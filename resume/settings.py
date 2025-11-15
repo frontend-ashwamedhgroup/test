b6b0eff7d58b02c59e3a40703c9f84bc455e32fb
vikas shinde <vikkyshinde02@gmail.com>
2025-11-15 15:26:25 +0530

    "again backe change"

--- resume/settings.py
+++ resume/settings.py
@@ -41,17 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'resume.urls'
@@ -128,7 +129,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # for development, optional
 
 # Directory where `collectstatic` will put all static files (for production)
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --------------------------
 # Media files settings
