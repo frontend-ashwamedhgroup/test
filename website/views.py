cd80e1db9a6add3b0ef605ee7f5eb757fcd7b226
vikas shinde <vikkyshinde02@gmail.com>
2025-11-15 16:55:12 +0530

    "remove unwanted urls and html files"

--- website/views.py
+++ website/views.py
@@ -4,10 +4,3 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html', {})
-
-def about(request):
-    return render(request,'about.html', {})
-
-
-def index(request):
-    return HttpResponse("Hello World")
